15dd390b6aad030ee7564295502d0d42a7c136ab
federico-copertini <fedecoper@hotmail.com>
2022-06-21 19:35:52 -0300

    agregamos urlde la app

--- ProyectoCoder/ProyectoCoder/urls.py
+++ ProyectoCoder/ProyectoCoder/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from ProyectoCoderApp.views import *
 
@@ -22,4 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio),
     path('crearpython/',crear_curso),
+
+    #URLs de ProyectoCoderAPP
+    path('coderapp/', include('ProyectoCoderApp.urls'))
 ]
